2451085f1301b6e7383c6b2b9bfe81c547ec576c
ankitranjan1028 <aankitrn17@gmail.com>
2025-07-19 02:03:30 +0530

    cors problem resolved

--- server/server.js
+++ server/server.js
@@ -12,19 +12,23 @@ connectDB();
 
 const app = express();
 
-const allowedOrigins = ["http://localhost:3000", process.env.CORS_ORIGIN];
+const allowedOrigins = [
+  "http://localhost:3000",
+  ...process.env.CORS_ORIGIN.split(",").map(origin => origin.trim())
+];
 
 const corsOptions = {
   origin: function (origin, callback) {
     if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg =
-        "The CORS policy for this site does not allow access from the specified Origin.";
-      return callback(new Error(msg), false);
+    if (!allowedOrigins.includes(origin)) {
+      console.log("Blocked Origin:", origin); // for debugging
+      return callback(new Error("CORS blocked"), false);
     }
     return callback(null, true);
   },
+  credentials: true
 };
+
 
 app.use(cors(corsOptions));
 
